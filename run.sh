bfcfb04394cf91bd245fc5dccbe196380cde8489
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2019-04-07 19:11:52 -0300

    Separa conversão para SQLite em outro script

--- run.sh
+++ run.sh
@@ -9,15 +9,9 @@ rm -rf $OUTPUT_PATH
 mkdir -p $OUTPUT_PATH
 
 time python tse.py headers
-time python tse.py candidatura \
-	--years=all --output=$OUTPUT_PATH/candidatura.csv.gz
-time python tse.py bemdeclarado \
-	--years=all --output=$OUTPUT_PATH/bemdeclarado.csv.gz
-time python tse.py votacao-zona \
-	--years=all --output=$OUTPUT_PATH/votacao-zona.csv.gz
-time rows csv2sqlite \
-	--schemas=headers/schema-candidatura.csv,headers/schema-bemdeclarado.csv,headers/schema-votacao-zona.csv \
-	$OUTPUT_PATH/candidatura.csv.gz \
-	$OUTPUT_PATH/bemdeclarado.csv.gz \
-	$OUTPUT_PATH/votacao-zona.csv.gz \
-	$DATA_PATH/eleicoes-brasil.sqlite
+
+time python tse.py candidatura
+time python tse.py bem-declarado
+time python tse.py votacao-zona
+time python tse.py receita
+time python tse.py despesa
