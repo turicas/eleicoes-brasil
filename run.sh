55c280b0a6d654e57be29909ce2a8a16d47a6850
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-06-06 03:13:07 -0300

    Melhora forma como mirror é criado

--- run.sh
+++ run.sh
@@ -8,10 +8,12 @@ OUTPUT_PATH=$DATA_PATH/output
 rm -rf $OUTPUT_PATH
 mkdir -p $OUTPUT_PATH
 
+OPTS=""  # or: OPTS="--use-mirror"
+
 time python tse.py headers
 
-time python tse.py candidatura
-time python tse.py bem-declarado
-time python tse.py votacao-zona
-time python tse.py receita
-time python tse.py despesa
+time python tse.py $OPTS candidatura
+time python tse.py $OPTS bem-declarado
+time python tse.py $OPTS votacao-zona
+time python tse.py $OPTS receita
+time python tse.py $OPTS despesa
